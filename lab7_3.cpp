fffacb1842b7a329d43d82d79495821edebdefa3
THENONT123_ <Nonthaphatc@hotmail.com_>
2024-12-06 15:16:03 +0700

    finished

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x == 'A'){
+		return x = 'Z';
+	}
+	else if(x>='A' && x<='Z'){
+		return x - 1;
+	}
+	return x = '0';
 }
 
 int main(){
